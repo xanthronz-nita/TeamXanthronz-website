83ab5d227443f616cf6f8882f24976effb018d5c
ganpati0123 <ganpatiraj716@gmail.com>
2025-12-27 12:31:24 +0600

    Refactor JoinUs component layout and styling

--- src/components/JoinUs.jsx
+++ src/components/JoinUs.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 export default function JoinUs() {
-  return <>
-    <h1 className="m-80 md:text-6xl font-semibold font-[orbitron]">this webpage is under construction</h1>
-  </>
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center px-4">
+      <h1 className="text-center text-2xl sm:text-3xl md:text-5xl lg:text-6xl
+      font-semibold font-[orbitron] text-white">
+        this webpage is under construction
+      </h1>
+    </div>
+  );
 }
