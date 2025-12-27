e2f1eae2ea6cea6ad628387ed12facc57bda23ca
ganpati0123 <ganpatiraj716@gmail.com>
2025-12-27 12:43:23 +0600

    Refactor Layout component structure
    Refactor Layout component to use a div wrapper and improve structure.

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
-    <>
-    <Header/>
-    <Outlet />
-    <Footer />
-    </>
-  )
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      
+      {/* Main content */}
+      <main className="flex-1 w-full">
+        <Outlet />
+      </main>
+
+      <Footer />
+    </div>
+  );
 }
 
-export default Layout
+export default Layout;
